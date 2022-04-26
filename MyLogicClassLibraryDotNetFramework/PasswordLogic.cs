5f92c9ee6e612bc84d6c3fddeb6ea91743a1b0e7
Unicow8 <tobiaslarsen93@gmail.com>
2022-04-26 14:40:50 +0200

    skrevet lidt logik, der måske giver mening

--- MyLogicClassLibraryDotNetFramework/PasswordLogic.cs
+++ MyLogicClassLibraryDotNetFramework/PasswordLogic.cs
@@ -8,5 +8,23 @@ namespace MyLogicClassLibraryDotNetFramework
 {
     public class PasswordLogic
     {
+        private string pwconditions;
+
+        public PasswordLogic(string pwconditions)
+        {
+            Pwconditions = pwconditions;
+        }
+
+        public string Pwconditions
+        {
+            get { return pwconditions; }
+            set 
+            {
+                if(value )
+                pwconditions = value; 
+            }   
+        }
+
+
     }
 }
